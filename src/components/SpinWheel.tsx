23660a8cbb778cd75dad2a7c0278293f5e9b2aa1
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-09-29 15:29:57 +0530

    buttonFix

--- src/components/SpinWheel.tsx
+++ src/components/SpinWheel.tsx
@@ -354,7 +354,9 @@ const SpinWheel: React.FC = () => {
                 <button
                   type="submit"
                   disabled={isLoading}
-                  className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 flex items-center justify-center"
+                  className={`px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 flex items-center justify-center ${
+                    isLoading ? " cursor-not-allowed" : "cursor-pointer"
+                  }`}
                 >
                   {isLoading ? <Loader /> : "Submit"}
                 </button>
